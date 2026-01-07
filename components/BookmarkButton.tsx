eab9246bc538d26b9a7ff67baf99afebaf688dd7
sangukO <woqls1972@gmail.com>
2026-01-08 03:16:42 +0900

    feat: 단어 상세 페이지에 북마크 버튼 추가
    - DB에서 해당 단어 북마크 여부 서버에서 보냄
    - 비회원 북마크 버튼 클릭 시 로그인 유도 커스텀 토스트 메시지 출력

--- components/BookmarkButton.tsx
+++ components/BookmarkButton.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import { toast } from "sonner";
+import ConfirmToast from "./ui/ConfirmToast";
+import { useRouter } from "next/navigation";
 
 interface Props {
   wordId: number;
@@ -10,6 +12,7 @@ interface Props {
   initialIsBookmarked?: boolean;
   // 상태가 바뀌면 부모에게 알림
   onChange?: (isBookmarked: boolean) => void;
+  disabled?: boolean;
 }
 
 export default function BookmarkButton({
@@ -17,12 +20,17 @@ export default function BookmarkButton({
   userId,
   initialIsBookmarked = false,
   onChange,
+  disabled,
 }: Props) {
   const supabase = createClient();
+  const router = useRouter();
+
   const [isBookmarked, setIsBookmarked] = useState(initialIsBookmarked);
   const [isLoading, setIsLoading] = useState(false);
 
   const toggleBookmark = async (e: React.MouseEvent) => {
+    if (disabled) return;
+
     e.preventDefault();
     e.stopPropagation();
 
@@ -58,10 +66,28 @@ export default function BookmarkButton({
         if (error) throw error;
         toast.success("단어장에 추가되었습니다.");
       }
-    } catch (error) {
-      console.error("북마크 에러:", error);
-      setIsBookmarked(previousState); // 롤백
-      toast.error("북마크 처리에 실패했습니다.");
+    } catch (error: any) {
+      if (error.code === "22P02") {
+        toast.custom(
+          (t) => (
+            <ConfirmToast
+              t={t}
+              title="로그인이 필요한 기능입니다."
+              description="로그인하시겠습니까?"
+              confirmLabel="로그인"
+              onConfirm={async () => {
+                router.push("/login");
+              }}
+            />
+          ),
+          { duration: Infinity, position: "top-center" }
+        );
+        setIsBookmarked(previousState); // 롤백
+      } else {
+        console.error("북마크 에러:", error);
+        setIsBookmarked(previousState); // 롤백
+        toast.error("북마크 처리에 실패했습니다.");
+      }
     } finally {
       setIsLoading(false);
     }
@@ -72,8 +98,14 @@ export default function BookmarkButton({
       onClick={toggleBookmark}
       className={`p-2 rounded-full transition-colors cursor-pointer ${
         isBookmarked ? "text-red-500" : "text-gray-400 hover:text-red-400 "
-      }`}
-      disabled={isLoading}
+      }
+      ${
+        disabled
+          ? "opacity-30 cursor-pointer pointer-events-none"
+          : "hover:scale-110 active:scale-95 cursor-pointer"
+      }
+        `}
+      disabled={isLoading || disabled}
       aria-label={isBookmarked ? "단어장 삭제" : "단어장 저장"}
     >
       <svg
