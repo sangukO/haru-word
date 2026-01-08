14933f1c4c665b5aaedb5097ee0debac3038f08d
sangukO <woqls1972@gmail.com>
2026-01-09 05:24:01 +0900

    feat: AI 생성 횟수 제한 관련 상수 정의

--- constants/service.ts
+++ constants/service.ts
@@ -1,2 +1,4 @@
 export const SERVICE_NAME = "하루단어";
 export const SERVICE_START_DATE = "2025-12-09";
+export const AI_DAILY_LIMIT = 3;
+export const AI_LIMIT_MESSAGE = `하루 이용 횟수(${AI_DAILY_LIMIT}회)를 모두 사용했습니다. 내일 다시 이용해주세요!`;
