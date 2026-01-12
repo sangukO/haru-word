45f516c7a456ad1bc59ac58faad9677f0a94e5d9
sangukO <woqls1972@gmail.com>
2026-01-13 04:57:38 +0900

    refactor: 날짜 공통 함수 추가

--- utils/date.ts
+++ utils/date.ts
@@ -24,6 +24,14 @@ export const getFormattedKoreanDate = (dateStr: string) => {
   return `${y}년 ${m}월 ${d}일`;
 };
 
+// Date 객체를 받아서 'YYYY-MM-DD' 형식으로 변환
+export const getFormatDateToString = (date: Date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
+
 // 날짜 더하거나 빼기
 export const offsetDate = (dateStr: string, days: number) => {
   const date = new Date(dateStr);
